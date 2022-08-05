7ea04316d69d5d58614e078a54c9196fe2162c0b
SimpleStudioCR <83027621+AndresMayorgaCR@users.noreply.github.com>
2022-08-05 15:56:00 -0600

    clean

--- src/app/pages/routing.ts
+++ src/app/pages/routing.ts
@@ -1,21 +1,10 @@
 import {Routes} from '@angular/router';
-import {ReportsModule} from "./reports/reports.module";
 
 const Routing: Routes = [
-  {
-    path: 'issuing',
-    loadChildren: () =>
-      import('./issuing/issuing.module').then((m) => m.IssuingModule),
-  },
   {
     path: 'users',
     loadChildren: () =>
       import('./user/user.module').then((m) => m.UserModule),
-  },
-  {
-    path: 'branch',
-    loadChildren: () =>
-      import('./branch/branch.module').then((m) => m.BranchModule),
   },
   {
     path: 'clients',
@@ -43,51 +32,9 @@ const Routing: Routes = [
       import('./reports/reports.module').then((m) => m.ReportsModule),
   },
   {
-    path: 'item-provider',
-    loadChildren: () =>
-      import('./item-provider/item-provider.module').then((m) => m.ItemProviderModule),
-  },
-  {
     path: 'dashboard',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
-  },
-  {
-    path: 'builder',
-    loadChildren: () =>
-      import('./builder/builder.module').then((m) => m.BuilderModule),
-  },
-  {
-    path: 'crafted/pages/profile',
-    loadChildren: () =>
-      import('../modules/profile/profile.module').then((m) => m.ProfileModule),
-  },
-  {
-    path: 'crafted/account',
-    loadChildren: () =>
-      import('../modules/account/account.module').then((m) => m.AccountModule),
-  },
-  {
-    path: 'crafted/pages/wizards',
-    loadChildren: () =>
-      import('../modules/wizards/wizards.module').then((m) => m.WizardsModule),
-  },
-  {
-    path: 'crafted/widgets',
-    loadChildren: () =>
-      import('../modules/widgets-examples/widgets-examples.module').then(
-        (m) => m.WidgetsExamplesModule
-      ),
-  },
-  {
-    path: 'apps/chat',
-    loadChildren: () =>
-      import('../modules/apps/chat/chat.module').then((m) => m.ChatModule),
-  },
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full',
   },
   {
     path: '**',
